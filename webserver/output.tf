e0dab41afe21436a13c71d88536ecf845a26d071
EC2 Default User <ec2-user@ip-172-31-46-75.ec2.internal>
2024-02-05 02:05:04 +0000

    webserver infrastructure created

---
+++ webserver/output.tf
@@ -0,0 +1,11 @@
+output "private_ip_ec2" {
+  value = module.web.private_ip_ec2
+}
+
+output "public_ip_ec2" {
+  value = module.web.public_ip_ec2
+}
+
+output "ec2_ids" {
+  value = module.web.ec2_ids
+}
